60233f0fc3a4661c7949c6c414a8e400496fd114
Elody <hwt@teamsz.xyz>
2020-05-05 17:59:13 +0800

    fix some error

--- util/feature_tool.py
+++ util/feature_tool.py
@@ -1,5 +1,10 @@
+import numpy as np
 import torch
+from numpy import linalg
+from torch.nn import Module, Conv2d, Parameter, Softmax
 import torch.nn.functional as F
+import sys
+import cv2
 
 # generate dense offsets feature 
 class FeatureModule:
@@ -15,8 +20,8 @@ class FeatureModule:
         img = F.interpolate(img, size = [feature_size, feature_size])
         jt_ft = jt_uvd.view(batch_size, -1, 1, 1).repeat(1, 1, feature_size, feature_size) # (B, joint_num*3, F, F)
 
-        mesh_x = 2.0 * torch.arange(feature_size).unsqueeze(0).expand(feature_size, feature_size).float() / (feature_size - 1.0) - 1.0
-        mesh_y = 2.0 * torch.arange(feature_size).unsqueeze(1).expand(feature_size, feature_size).float() / (feature_size - 1.0) - 1.0
+        mesh_x = 2.0 * (torch.arange(feature_size).unsqueeze(0).expand(feature_size, feature_size).float() + 0.5) / (feature_size - 1.0) - 1.0
+        mesh_y = 2.0 * (torch.arange(feature_size).unsqueeze(1).expand(feature_size, feature_size).float() + 0.5) / (feature_size - 1.0) - 1.0
         coords = torch.stack((mesh_x, mesh_y), dim=0)
         coords = coords.unsqueeze(0).repeat(batch_size, 1, 1, 1).to(jt_uvd.device) # (B, 2, F, F)
         coords = torch.cat((coords, img), dim=1).repeat(1, jt_num, 1, 1) # (B, jt_num*3, F, F)
@@ -24,13 +29,14 @@ class FeatureModule:
         offset = jt_ft - coords # (B, jt_num*3, F, F)
         offset = offset.view(batch_size, jt_num, 3, feature_size, feature_size) # (B, jt_num, 3, F, F)
         dis = torch.sqrt(torch.sum(torch.pow(offset, 2), dim=2) + 1e-8) # (B, jt_num, F, F)
-        offset_norm = offset / dis.unsqueeze(2) # (B, jt_num, 3, F, F)
 
+        offset_norm = offset / dis.unsqueeze(2) # (B, jt_num, 3, F, F)
         heatmap = (kernel_size - dis) / kernel_size # (B, jt_num, F, F)
-        mask = (heatmap.ge(0) * img.lt(0.99)).float() # (B, jt_num, F, F)
-        offset_norm =  (offset_norm * mask.unsqueeze(2)).view(batch_size, -1, feature_size, feature_size)
-        heatmap *= mask
-        return torch.cat((offset_norm, heatmap), dim=1).float()
+        mask = heatmap.ge(0).float() * img.lt(0.99).float() # (B, jt_num, F, F)
+
+        offset_norm_mask =  (offset_norm * mask.unsqueeze(2)).view(batch_size, -1, feature_size, feature_size)
+        heatmap_mask = heatmap * mask.float()
+        return torch.cat((offset_norm_mask, heatmap_mask), dim=1).float()
 
     def offset2joint_softmax(self, offset, img, kernel_size):
         batch_size, feature_num, feature_size, _ = offset.size()
@@ -41,22 +47,21 @@ class FeatureModule:
         # closeness heatmap
         offset_ht = offset[:, jt_num*3:].contiguous() # (B, jt_num, F, F)
 
-        mesh_x = 2.0 * torch.arange(feature_size).unsqueeze(0).expand(feature_size, feature_size).float() / (feature_size - 1.0) - 1.0
-        mesh_y = 2.0 * torch.arange(feature_size).unsqueeze(1).expand(feature_size, feature_size).float() / (feature_size - 1.0) - 1.0
+        mesh_x = 2.0 * (torch.arange(feature_size).unsqueeze(0).expand(feature_size, feature_size).float() + 0.5) / (feature_size - 1.0) - 1.0
+        mesh_y = 2.0 * (torch.arange(feature_size).unsqueeze(1).expand(feature_size, feature_size).float() + 0.5) / (feature_size - 1.0) - 1.0
         coords = torch.stack((mesh_x, mesh_y), dim=0)
         coords = coords.unsqueeze(0).repeat(batch_size, 1, 1, 1).to(offset.device)
         coords = torch.cat((coords, img), dim=1).repeat(1, jt_num, 1, 1) # (B, jt_num*3, F, F)
         coords = coords.view(batch_size, jt_num, 3, -1) # (B, jt_num, 3, F*F)
 
         mask = img.lt(0.99).float() # (B, 1, F, F)
-        offset_vec = (offset_vec * mask).view(batch_size, jt_num, 3, -1) # (B, jt_num, 3, F*F)
-        offset_ht =  (offset_ht * mask).view(batch_size, jt_num, -1) # (B, jt_num, F*F)
-        offset_ht = F.softmax(offset_ht * 30, dim = -1) # (B, jt_num, F*F)
-        dis = kernel_size - offset_ht * kernel_size # (B, jt_num, F*F)
+        offset_vec_mask = (offset_vec * mask).view(batch_size, jt_num, 3, -1) # (B, jt_num, 3, F*F)
+        offset_ht_mask =  (offset_ht * mask).view(batch_size, jt_num, -1) # (B, jt_num, F*F)
+        offset_ht_norm = F.softmax(offset_ht_mask * 30, dim = -1) # (B, jt_num, F*F)
+        dis = kernel_size - offset_ht_mask * kernel_size # (B, jt_num, F*F)
 
-        jt_uvd = torch.sum((offset_vec * dis.unsqueeze(2) + coords) * offset_ht.unsqueeze(2), dim=-1)
+        jt_uvd = torch.sum((offset_vec_mask * dis.unsqueeze(2) + coords) * offset_ht_norm.unsqueeze(2), dim=-1)
 
         return jt_uvd.float()
-
 
 
